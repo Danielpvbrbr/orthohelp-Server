6ab48a334a4ab12314949917ca3cf5de4658e7d3
Danielpvbrbr <dntechpvbr@gmail.com>
2023-08-04 12:36:15 -0300

    Atualizando as credencial do banco de dados

--- connection.js
+++ connection.js
@@ -1,12 +1,19 @@
 const mysql = require('mysql2');
-
 const con = mysql.createConnection({
-    host: "localhost",
+    host: "189.126.111.161",
     port: 3306,
-    user: "root",
-    password: "",
+    user: "orthohelp",
+    password: "Orthohelp@Orthohelp2023",
     database: "orthohelp"
 });
+
+// const con = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "",
+//     database: "orthohelp"
+// });
 
 
 module.exports.con = con;
