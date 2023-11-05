d6fc44abeaad88ddac07b05c425b923075dc120a
Danielpvbrbr <dntechpvbr@gmail.com>
2023-11-05 16:34:01 -0300

    Alterando a string de conexão do banco

--- connection.js
+++ connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
-// const con = mysql.createConnection({
-//     host: "189.126.111.161",
-//     port: 3306,
-//     user: "orthohelp",
-//     password: "Orthohelp@Orthohelp2023",
-//     database: "orthohelp"
-// });
+const con = mysql.createConnection({
+    host: "189.126.111.161",
+    port: 3306,
+    user: "orthohelp",
+    password: "Orthohelp@Orthohelp2023",
+    database: "orthohelp"
+});
 
 // const con = mysql.createConnection({
 //     host: "localhost",
@@ -15,12 +15,12 @@ const mysql = require('mysql2');
 //     database: "orthohelp_prod"
 // });
 
-const con = mysql.createConnection({
-    host: "mysql32-farm10.kinghost.net",
-    port: 3306,
-    user: "orthohelp_add1",
-    password: "dev1801",
-    database: "orthohelp"
-});
+// const con = mysql.createConnection({
+//     host: "mysql32-farm10.kinghost.net",
+//     port: 3306,
+//     user: "orthohelp_add1",
+//     password: "dev1801",
+//     database: "orthohelp"
+// });
 
 module.exports.con = con;
